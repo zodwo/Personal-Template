3ecfa2420d93c633d4c533f8f5be48d442f81a2a
zodwo <senanm260@gmail.com>
2023-06-28 10:19:21 +0400

    menu fixed activation

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,32 +1,111 @@
 const menuItem = document.querySelectorAll(".menu-links li a");
 const header = document.querySelector("#header");
 const container = header.querySelector(".container");
-const deletedItem = document.querySelectorAll(".deleted")
+const deletedItem = document.querySelectorAll(".deleted");
+const body = document.querySelector("body");
 
+let windowWIDTH = window.innerWidth;
 
-function centerToTop(){
+function CheckWinowSize() {
+    if (windowWIDTH > 991) {
+        centerToTop();
+    } else {
+        // ChechkMobileSizeForDeletedItem();
+        // container.style.display = "block";
+        header.style.backgroundColor = "transparent";
+    }
+}
+
+function RemoveHeightHEADER() {
+    header.classList.remove("header-min-height");
+    header.classList.add("header-max-height");
+}
+function addHeightHEADER() {
+    header.classList.add("header-min-height");
+    header.classList.remove("header-max-height");
+}
+
+function deletedItemDn() {
+    deletedItem.forEach((item) => {
+        item.classList.add("display-nonee");
+    });
+}
+
+function showDeletedItem() {
+    deletedItem.forEach((item) => {
+        item.classList.remove("display-nonee");
+    });
+}
+
+function centerToTop() {
     menuItem.forEach((element) => {
         element.addEventListener("click", (e) => {
             if (e.target.textContent != "Home") {
-                header.classList.add("header-top");
-                header.style.height = "min-content";
-                container.style.display = "flex";
-                deletedItem.forEach(item =>{
-                    item.style.display = 'none'
-                })
-                
+                notEqualToHOME();
             } else {
-                header.classList.remove("header-top");
-                header.style.height = "100%";
-                container.style.display = "block";
-                deletedItem.forEach(item =>{
-                    item.style.display = 'flex'
-                })
+                EqualToHOME();
             }
         });
     });
 }
 
-centerToTop()
+function notEqualToHOME() {
+    container.classList.add("flex-boxsm");
+    container.classList.remove("display-block");
 
-window.addEventListener("resize",centerToTop)
+    header.classList.add("header-top");
+
+    addHeightHEADER();
+    deletedItemDn();
+    // checkContainerClass()
+}
+
+function EqualToHOME() {
+    container.classList.remove("flex-boxsm");
+    container.classList.add("display-block");
+
+    header.classList.remove("header-top");
+    body.classList.remove("scroll");
+
+    RemoveHeightHEADER();
+    showDeletedItem();
+    // checkContainerClass()
+}
+
+function ChechkMobileSizeForDeletedItem(e) {
+    if (windowWIDTH < 991) {
+        if (e != "Home") {
+            body.classList.add("scroll");
+            deletedItemDn();
+            addHeightHEADER();
+            headTEXT.classList.remove("head-text");
+
+        } else {
+            body.classList.remove("scroll");
+            showDeletedItem();
+            RemoveHeightHEADER();
+        }
+    } else {
+        showDeletedItem();
+        RemoveHeightHEADER();
+        navbar.classList.remove("DeactiveNAV");
+        // navbar.classList.add("activeNAV"); sil bunu
+    }
+}
+
+menuItem.forEach((items) => {
+    items.addEventListener("click", (it) => {
+        if (windowWIDTH < 991) {
+            ChechkMobileSizeForDeletedItem(it.target.textContent);
+            console.log(it.target.textContent);
+        }
+    });
+});
+
+CheckWinowSize();
+
+window.addEventListener("resize", () => {
+    windowWIDTH = window.innerWidth;
+    CheckWinowSize();
+    // ChechkMobileSizeForDeletedItem();
+});
--- assets/js/mobile-menu.js
+++ assets/js/mobile-menu.js
@@ -1,7 +1,73 @@
 const menuIcons = document.querySelector(".menu-toggle");
+const navbar = document.querySelector(".navbar");
+const closeMenuIcons = document.querySelector(".x-block");
 
-menuIcons.addEventListener("click", (e) => {
-    menuIcons.classList.add("active");
+window.addEventListener("resize", () => {
+    CheckSize2();
 });
 
+CheckSize2();
 
+function CheckSize2() {
+    //
+    if (window.innerWidth < 991) {
+        navbar.classList.remove("window991pxNAVBAR")
+        header.classList.remove("header-top");
+
+        // showDeletedItem();
+        menuIcons.addEventListener("click", () => {
+            menuIcons.classList.add("active");
+            CheckIsActive();
+        });
+
+
+        function CheckIsActive() {
+            if (menuIcons.classList.contains("active")) {
+                navbar.classList.remove("DeactiveNAV")
+                navbar.classList.add("activeNAV")
+                closeMenuIcons.style.display = "block";
+                // console.log("object1");
+            } else if(!menuIcons.classList.contains("active")){
+                navbar.classList.remove("activeNAV")
+                navbar.classList.add("DeactiveNAV")
+                closeMenuIcons.style.display = "none";
+                header.style.height = "100vh";
+                // console.log("object2");
+            }
+        }
+
+        function CloseButton() {
+            menuIcons.classList.remove("active");
+            CheckIsActive();
+        }
+
+        closeMenuIcons.addEventListener("click", CloseButton);
+        // Call function
+        CheckIsActive();
+    } else {
+        navbar.classList.add("window991pxNAVBAR")
+        menuIcons.classList.remove("active");
+        closeMenuIcons.style.display = "none";
+    }
+}
+
+/// ************************************
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((a) => {
+    a.addEventListener("click", () => {
+        if (!a.classList.contains("activex")) {
+            navLink.forEach((active) => {
+                active.classList.remove("activex");
+            });
+        }
+
+        a.classList.add("activex");
+    });
+});
+
+//*************************************** */
+navbar.addEventListener("click", () => {
+    menuIcons.classList.remove("active");
+    CheckSize2();
+});
